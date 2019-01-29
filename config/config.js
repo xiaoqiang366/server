12bd8c5a84989e3973367924ecb2b9aaa9474c03
敖晓强 <aoxiaoqiang@163.com>
2019-01-29 23:56:29 +0800

    fix: ip获取

--- config/config.js
+++ config/config.js
@@ -4,5 +4,25 @@ module.exports = {
   mongoPort: 27040, // mongodb端口
   jwtSecret: 'seller-secret', // 连接端口
 
-  imagePath: `http://127.0.0.1:3030`
+  imagePath: `http://${getIp()}:3030`
+}
+
+function getIp() {
+  let os = require('os'),
+    ipStr,
+    infaces = os.networkInterfaces(),
+    bool = false;
+  for (let i in infaces) {
+    infaces[i].some(function (x) {
+      if ((x.family == 'IPv4') && (x.internal == false)) {
+        ipStr = x.address;
+        bool = true;
+        return true
+      }
+    })
+    if (bool) {
+      break
+    }
+  }
+  return ipStr
 }
