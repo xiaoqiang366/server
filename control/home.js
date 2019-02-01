3c5e477ec7499b3848775aea1c1fc58854f602d1
敖晓强 <aoxiaoqiang@163.com>
2019-02-01 21:54:17 +0800

    feat: system

--- control/home.js
+++ control/home.js
@@ -11,7 +11,10 @@ class Home {
   system(ctx, next) {
     ctx.send({
       version: '0.0.1',
-      sysname: 'xxx系统',
+      sysname: '点餐后台管理系统',
+      logo: 'http://116.62.147.91:3030/img/logo.png',
+      slogon: '2019学习路上',
+      desc: '本项目是一款前后端分离全栈开发项目，本次项目做一个进店点餐系统，从一个真实的快餐店，饭店等角度出发。实现手机点餐的功能。前端采用 vue 框架构建,后台使用 Koa2 框架构建,MongoDB 数据库,开发过程为 git 分支管理多人协作开发。'
     })
     next();
   }
